1bc17fa858442eafa3de85e3a23cb7dac497f2bc
alan <2194999896@qq.com>
2016-12-10 18:02:50 +0800

    add rotate function

--- src/main/java/DataStructureImpl/RedBlackTree.java
+++ src/main/java/DataStructureImpl/RedBlackTree.java
@@ -11,8 +11,7 @@ package DataStructureImpl;
  *
  * Created by alan on 16/4/5.
  */
-//TODO
-public class BlackRedTree<Key extends  Comparable<Key>>{
+public class RedBlackTree<Key extends  Comparable<Key>>{
 
     private final static boolean RED=true;
     private final static boolean BLACK=false;
@@ -21,7 +20,7 @@ public class BlackRedTree<Key extends  Comparable<Key>>{
         Key key;
         int val;
         Node left,right;
-        int N;
+        int N;             //以该节点为根自节点的总数
         boolean color;
 
         public Node(Key key, int val, Node left, Node right, int n, boolean color) {
@@ -39,5 +38,49 @@ public class BlackRedTree<Key extends  Comparable<Key>>{
         if(x==null) return false;
         return x.color==RED;
     }
+
+
+    /**
+     *  左旋
+     */
+    public Node rotateLeft(Node h){
+        //交换左右链接
+        Node x=h.right;
+        h.right=x.left;
+        x.left=h;
+
+         //改变连接颜色
+        x.color=h.color;
+        h.color=RED;
+
+        //改变连接的自节点总数
+        x.N=h.N;
+        h.N=1+size(h.right)+size(h.right);
+
+        return x;
+    }
+
+    /**
+     *  右旋
+     */
+    public Node rotateRight(Node h){
+        Node x=h.left;
+        h.left=x.right;
+        x.right=h;
+
+        x.color=h.color;
+        h.color=RED;
+
+        x.N=h.N;
+        h.N=1+size(h.right)+size(h.right);
+        return  x;
+    }
+
+
+    public int size(Node node){
+        if(node==null) return 0;
+        return node.N;
+    }
+
 
 }
