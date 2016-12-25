3b25362bd97d092ca4024b5f2513867f1a1d2ed8
alan <2194999896@qq.com>
2016-12-25 18:13:38 +0800

    添加PrimMST lazy实现

--- src/main/java/LazyPrimMST.java
+++ src/main/java/LazyPrimMST.java
@@ -8,7 +8,9 @@ import java.util.Queue;
 
 /**
  *
- * 最小生成树的Prim时延
+ * 最小生成树的Prim时延实现
+ *      时间：ELogE(worst)
+ *
  * Created by alan on 16/12/24.
  */
 public class LazyPrimMST {
@@ -23,8 +25,19 @@ public class LazyPrimMST {
         marked=new boolean[G.V()];
         mst=new LinkedList<>();
 
-        visit(G,0);
+        visit(G,0);    //假设G是连通的
 
+        while (!pq.isEmpty()){
+            Edge e=pq.delMin();    //从pq中得到最小的边
+
+            int v=e.either(),w=e.other(v);
+            if(marked[v] && marked[w]) continue;  //跳过失效的边
+            mst.add(e);                           //将边添加到树中
+            if(!marked[v])
+                visit(G,v);
+            if(!marked[w])
+                visit(G,w);
+        }
     }
 
     private void visit(EdgeWeightGraph G, int v) {
@@ -32,6 +45,11 @@ public class LazyPrimMST {
         for (Edge e:G.adj(v))
             if(!marked[e.other(v)])
                 pq.insert(e);
+    }
+
+
+    public Iterable<Edge> edges(){
+        return mst;
     }
 
 
