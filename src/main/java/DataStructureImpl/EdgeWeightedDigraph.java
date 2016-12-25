738468f41a10e2926472da9d28a0631f9adb6322
alan <2194999896@qq.com>
2016-12-25 20:54:48 +0800

    部分添加有向加权图的api实现

---
+++ src/main/java/DataStructureImpl/EdgeWeightedDigraph.java
@@ -0,0 +1,53 @@
+package DataStructureImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * 加权有向图
+ * Created by alan on 16/12/25.
+ */
+public class EdgeWeightedDigraph {
+
+    private final int V;
+    private int E;
+    private List<DirectedEdge>[] adj;
+
+
+    public EdgeWeightedDigraph(int V) {
+        this.V = V;
+        this.E=0;
+        adj=new List[V];
+        for (int v=0;v<V;v++)
+            adj[v]=new ArrayList<DirectedEdge>();
+    }
+
+    public int V(){
+        return V;
+    }
+
+
+    public int E(){
+        return E;
+    }
+
+    public void addEdge(DirectedEdge e){
+        adj[e.from()].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v){
+        return adj[v];
+    }
+
+    public Iterable<DirectedEdge> edges(){
+        List<DirectedEdge> bag=new ArrayList<>();
+        for(int v=0;v<V;v++)
+            for (DirectedEdge e:adj[v])
+                bag.add(e);
+        return bag;
+    }
+
+
+}
